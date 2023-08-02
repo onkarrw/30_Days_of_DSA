e17cee9911aacb80142d4e7e082c676cfbe8ef31
ONKAR WAGH <86963691+onkarrw@users.noreply.github.com>
2023-08-02 22:34:31 +0530

    Add files via upload

---
+++ Occurences.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int arr[] = {2, 5, 7, 8, 1, 4, 6, 2};
+
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int x;
+    int counter = 0;
+     cin >> x;
+
+
+     for (int i= 0  ; i< n; i++)
+     {
+        if (arr[i] == x)
+        {
+            counter++;
+        }
+     }
+
+
+     cout << x << " occurred " << counter  << " times.";
+}
