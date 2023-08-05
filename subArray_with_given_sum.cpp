c036163bcb32695407f60cb2b2907529bcaeb21b
ONKAR WAGH <86963691+onkarrw@users.noreply.github.com>
2023-08-05 23:46:00 +0530

    Update subArray_with_given_sum.cpp

--- subArray_with_given_sum.cpp
+++ subArray_with_given_sum.cpp
@@ -1,3 +1,9 @@
+/* 
+
+	Given an array arr[] of non-negative integers and an integer sum, find a subarray that adds to a given sum. There may be more than one subarray with sum as the given sum, print first such subarray.
+*/
+
+
 #include <iostream>
 #include <vector>
 using namespace std;
